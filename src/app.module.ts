7cc7e5bfec3d7508a27f5c635be13e73b6c492b4
Andre Aguayo <andre.aguayo@hotmail.com>
2023-02-24 02:37:58 -0300

    fix: translate into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
+import { TranslateModule } from './modules/common/translate/translate.module';
+import { HealthCheckModule } from './modules/health-check/health-check.module';
+import { TranslateService } from './modules/common/translate/translate.service';
+import './language/pt/messages.json';
+import './language/en/messages.json';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'pt',
+      loaderOptions: {
+        path: join(__dirname, '/language/'),
+        watch: true,
+      },
+      resolvers: [AcceptLanguageResolver],
+    }),
+    TranslateModule,
+    HealthCheckModule,
+  ],
+  providers: [TranslateService],
+  controllers: [],
 })
 export class AppModule {}
