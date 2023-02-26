507da0f23d1ea2e778dfd86622ebdbdbf89f8b36
Andre Aguayo <andre.aguayo@hotmail.com>
2023-02-25 22:08:13 -0300

    fix: adding swagger into main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import config from './config/config';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Weef')
+    .setDescription('API de teste para weef')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(config.api.port);
 }
 bootstrap();
