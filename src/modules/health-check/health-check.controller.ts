b951ed5165777a8f0de67d29c38df38ca0067886
Andre Aguayo <andre.aguayo@hotmail.com>
2023-02-25 22:14:53 -0300

    doc: adding role into health-check function

--- src/modules/health-check/health-check.controller.ts
+++ src/modules/health-check/health-check.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { HealthCheckService } from './health-check.service';
 import routes from '../../config/routes';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '../common/enums/role.enum';
 
 @Controller(routes.healthCheck.alias)
 @ApiTags('health check')
@@ -9,6 +11,7 @@ export class HealthCheckController {
   constructor(private readonly healthCheckService: HealthCheckService) {}
 
   @Get()
+  @Roles(Role.PUBLIC)
   @ApiResponse({
     status: HttpStatus.OK,
     type: String,
